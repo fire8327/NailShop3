ef33f6ab36ecd58cf37c6e1c28ad644ae77f3ce6
fire8327 <fire83274@gmail.com>
2025-05-02 03:03:00 +0300

    upd header

---
+++ Assets/JS/main.js
@@ -0,0 +1,11 @@
+$("#toggler").click(() => {
+    $("#menu").toggleClass("max-lg:top-0 max-lg:-translate-y-full")
+    $("#menu").toggleClass("max-lg:top-full")
+    $("#overlay").toggleClass("hidden")
+})
+
+$("#overlay").click(() => {
+    $("#menu").addClass("max-lg:top-0 max-lg:-translate-y-full")
+    $("#menu").removeClass("max-lg:top-full")
+    $("#overlay").addClass("hidden")
+})
